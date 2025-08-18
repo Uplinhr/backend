78d41d5f0a5c36c8453b0a74981263bf205e0876
elpista <marcopistagnesi2003@gmail.com>
2025-08-18 18:36:01 -0300

    Sistema de seeders y seeders de usuarios creados

---
+++ src/database/seeders/01-usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (nombre, apellido, email, contrasenia, estado, rol) VALUES
+('admin', 'admin', 'admin@gmail.com', '$2b$10$ArR7gGkUxWPiA1xhZCNhf.7OSBbAinoz5foLFJsNEg7rMYPqiZaCm', 'activo', 'admin'),
+('test', 'suarez', 'test@gmail.com', '$2b$10$CDrrmmJ0ecYVBCkQwtWI8./bv3rFagjE46Wj9kPITJP7qZloUeCOa', 'activo', 'cliente'),
+('testDisable', 'gomez', 'test2@gmail.com', '$2b$10$CDrrmmJ0ecYVBCkQwtWI8./bv3rFagjE46Wj9kPITJP7qZloUeCOa', 'inactivo', 'cliente');
