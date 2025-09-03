3ecba4524e27c4c18b48f54a7afd00b0fdb44782
elpista <marcopistagnesi2003@gmail.com>
2025-09-03 19:32:34 -0300

    Rutas reinicioContrasenia

--- src/features/auth/routes.js
+++ src/features/auth/routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout, editPassword, requestPasswordReset } from './controller.js';
+import { register, login, logout, editPassword, requestPasswordReset, validateResetToken, resetPassword } from './controller.js';
 import { authRequired, checkRole } from '../../middlewares/auth.js';
 
 const router = express.Router();
@@ -8,7 +8,12 @@ router.post('/register', register, authRequired, checkRole(['admin']));
 router.post('/login', login);
 router.post('/logout', logout);
 router.put('/editPassword/:id', authRequired, editPassword)
+
+
 router.post('/forgotPassword', requestPasswordReset)
+router.post('/validateToken', validateResetToken)
+router.post('/resetPassword', resetPassword)
+
 
 //Falta mailRecoverPassword(investigar enviar mails) y recoverPassword(investigar webhooks)
 
